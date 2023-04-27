a7bd34bc26032edd7309f274314e6e5c4d9fcdb1
cdelavegamartin <c.delavega.martin@protonmail.com>
2023-04-27 01:29:56 +0100

    Move get_t60() to platefno.util.conf module

--- platefno/util/conf.py
+++ platefno/util/conf.py
@@ -9,8 +9,25 @@ def print_hydra_config(cfg: DictConfig) -> None:
     return
 
 
-def get_config(dir_name):
-    return OmegaConf.load(os.path.join(dir_name, ".hydra", "config.yaml"))
+def get_config(dir_name, config_name="config"):
+    return OmegaConf.load(os.path.join(dir_name, ".hydra", f"{config_name}.yaml"))
+
+
+# function to convert from the T60 format in the config to the one required by the solver
+def get_t60(cfg):
+    # Set T60 depending on the damping configuration
+    if cfg.solver.damping.name == "nodamping":
+        t60 = None
+    elif cfg.solver.damping.name == "constant":
+        t60 = cfg.solver.damping.T60
+    elif cfg.solver.damping.name == "freqdependent":
+        t60 = (
+            {"f": cfg.solver.damping.f1, "T60": cfg.solver.damping.T60f1},
+            {"f": cfg.solver.damping.f2, "T60": cfg.solver.damping.T60f2},
+        )
+    else:
+        raise ValueError("Invalid damping configuration")
+    return t60
 
 
 if __name__ == "__main__":
